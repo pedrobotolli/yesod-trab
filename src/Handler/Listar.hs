b98770baf8b19aeb4dfb04d2bcf6fb19106242bf
romefeller <pedrobotolli.santos@gmail.com>
2017-12-05 11:08:29 +0000

    método Listar está passando os prestadores e suas respectivas profissões em uma tupla, profissão adicionada na infowindow do mapa - Narciso

--- src/Handler/Listar.hs
+++ src/Handler/Listar.hs
@@ -13,5 +13,14 @@ import Database.Persist.Postgresql
 
 getListarR :: Handler Value
 getListarR = do
-    rows <- runDB $ selectList [] [] :: Handler [Entity Prestador]
-    sendStatusJSON ok200 (object ["resp" .= (toJSON rows)])
+    prepro <- runDB $ selectList [] [] :: Handler [Entity PrestProfi]
+    
+    prestprofi <- return $ fmap (\(Entity _ pre) -> pre) prepro --Handler [Prestprofi]
+    prestids <- return $ fmap prestProfiPrestadorId prestprofi --[Handler ProfissaoId]
+    prestadores <- sequence $ fmap (\prestid -> runDB $ get404 prestid) prestids -- Handler [Profissoes]
+    
+    profids <- return $ fmap prestProfiProfissaoId prestprofi --[Handler ProfissaoId]
+    profissoes <- sequence $ fmap (\profid -> runDB $ get404 profid) profids -- Handler [Profissoes]
+    prestadorprofissao <- return $ zip prestadores profissoes
+
+    sendStatusJSON ok200 (object ["resp" .= (toJSON prestadorprofissao)])
