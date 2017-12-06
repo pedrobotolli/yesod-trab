ed0b9db0c32ad897dd94e959cef86b5bb67d8f8e
romefeller <pedrobotolli.santos@gmail.com>
2017-12-06 22:21:54 +0000

    foi criado a estrutura da pagina de remoçao de profissoes - Pedro, Gabriel e Narciso

--- src/Handler/Profissoes.hs
+++ src/Handler/Profissoes.hs
@@ -54,3 +54,40 @@ postAdicionarProfiR pid = do
             runDB $ insert $ PrestProfi (prestProfiProfissaoId profissao) pid
             redirect PrestadorR
         _ -> redirect HomeR
+        
+getRemoverProfiR :: PrestadorId -> Handler Html
+getRemoverProfiR pid = do
+    defaultLayout $ do
+        setTitle "Service Provider Finder"
+        [whamlet|
+            <section id="portfolio">
+                <div class="container">
+                    <div class="row text-center">
+                        <h2>Denúncias
+                        <legend>
+                    
+                    <div class="col-md-8 col-md-offset-2 table-responsive">  
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <td> 
+                                        Nome do Prestador
+                                    <td>
+                                        Data da Denúncia
+                                    <td>
+                                        Descrição da Denúncias
+                            <tbody>
+                                $forall (Entity did denuncia, prestador) <- prestadordenuncia
+                                    <tr id=#{show $fromSqlKey did}> 
+                                        <td>#{prestadorNomePrest prestador}
+                                        <td>#{show $ denunciaDtDenuncia denuncia}
+                                        <td>#{denunciaDsDenuncia denuncia}
+                                        <td><buttom type="buttom" onclick="deletarPrestador(this,'@{DenunciaR $ denunciaPrestadorId denuncia}')"  class="btn btn-danger" >Banir 
+            
+                          
+                          
+                        <a class="btn btn-secondary btn-lg pull-right" href=@{ListaContatoR}>Mensagens
+
+
+        
+        |]
